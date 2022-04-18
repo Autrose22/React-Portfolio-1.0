a07d753693f21e2dc283c287df8bf530007f7c32
autumnrose <Arosebarrios95@gmail.com>
2022-04-17 22:17:24 -0500

    more stuff

--- develop/src/app.js
+++ develop/src/app.js
@@ -1,1 +1,7 @@
 import React from 'react';
+import AboutMe from './components/AboutMe';
+import ContactInfo from './components/ContactInfo';
+import Intro from './components/Intro';
+import Resume from './components/Resume';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
